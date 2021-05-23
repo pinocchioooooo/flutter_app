edb4362bc29d0de4472a8f7ad34cf0262a0eb456
Pinocchio <1096166241@qq.com>
2021-05-23 20:23:13 +0800

    :bug: 修复键盘弹起背景图片错位问题

--- lib/screens/login/login_screen.dart
+++ lib/screens/login/login_screen.dart
@@ -8,6 +8,7 @@ class LoginScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      resizeToAvoidBottomInset: false,
       body: SafeArea(
         child: Background(
           child: Center(
